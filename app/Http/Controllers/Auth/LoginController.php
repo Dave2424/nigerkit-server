360792eb53cd19aea5d80b048aea83be68b9cbb7
dave <david.ifeanyi84@gmail.com>
2021-01-18 23:20:06 +0100

    new update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -45,6 +45,7 @@ class LoginController extends Controller
 
     public function login( Request $request )
     {
+        
         $this->validateLogin($request);
         $admin=Admin::where('email',$request->email)->first();
 
